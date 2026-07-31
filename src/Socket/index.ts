ee41c1f2a54663f3cfc7006b363b87d81b3114f7
Neykoor <cafecafirexos@gmail.com>
2026-07-30 21:09:33 -0700

    Update index.ts

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -2,11 +2,19 @@ import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type { UserFacingSocketConfig } from '../Types'
 import { makeCommunitiesSocket } from './communities'
 
-// export the last socket layer
+
 const makeWASocket = (config: UserFacingSocketConfig) => {
 	const newConfig = {
 		...DEFAULT_CONNECTION_CONFIG,
 		...config
+	}
+
+	if (config.version && config.syncWaWebVersion === undefined) {
+		newConfig.syncWaWebVersion = false
+	}
+
+	if (!newConfig.version) {
+		newConfig.version = DEFAULT_CONNECTION_CONFIG.version
 	}
 
 	return makeCommunitiesSocket(newConfig)
