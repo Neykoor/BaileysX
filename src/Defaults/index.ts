ba8655759dd11c97240ab1bcd0a50f01edb3f945
Neykoor <cafecafirexos@gmail.com>
2026-07-30 21:16:59 -0700

    Update index.ts

--- src/Defaults/index.ts
+++ src/Defaults/index.ts
@@ -22,16 +22,14 @@ export const WA_ADV_HOSTED_DEVICE_SIG_PREFIX = Buffer.from([6, 6])
 
 export const WA_DEFAULT_EPHEMERAL = 7 * 24 * 60 * 60
 
-
 export const STATUS_EXPIRY_SECONDS = 24 * 60 * 60
-
 
 export const PLACEHOLDER_MAX_AGE_SECONDS = 14 * 24 * 60 * 60
 
 export const NOISE_MODE = 'Noise_XX_25519_AESGCM_SHA256\0\0\0\0'
 export const DICT_VERSION = 3
 export const KEY_BUNDLE_TYPE = Buffer.from([5])
-export const NOISE_WA_HEADER = Buffer.from([87, 65, 6, DICT_VERSION]) 
+export const NOISE_WA_HEADER = Buffer.from([87, 65, 6, DICT_VERSION])
 
 export const URL_REGEX = /https:\/\/(?![^:@\/\s]+:[^:@\/\s]+@)[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?/g
 
@@ -60,6 +58,7 @@ export const DEFAULT_CACHE_TTLS = {
 
 export const DEFAULT_CONNECTION_CONFIG: SocketConfig = {
 	version: version as WAVersion,
+	syncWaWebVersion: true,
 	browser: Browsers.macOS('Chrome'),
 	waWebSocketUrl: 'wss://web.whatsapp.com/ws/chat',
 	connectTimeoutMs: 20_000,
@@ -141,14 +140,13 @@ export type MediaType = keyof typeof MEDIA_HKDF_KEY_MAPPING
 
 export const MEDIA_KEYS = Object.keys(MEDIA_PATH_MAP) as MediaType[]
 
-
 export const HISTORY_SYNC_PAUSED_TIMEOUT_MS = 120_000
 
 export const MIN_PREKEY_COUNT = 5
 
 export const INITIAL_PREKEY_COUNT = 812
 
-export const UPLOAD_TIMEOUT = 30000 
+export const UPLOAD_TIMEOUT = 30000
 
 export const BIZ_BOT_SUPPORT_PAYLOAD = JSON.stringify({
 	version: 1,
