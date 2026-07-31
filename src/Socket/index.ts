af343994fee7c0e1bdefaaa93c8e828f681459e9
Neykoor <cafecafirexos@gmail.com>
2026-07-30 22:13:50 -0700

    Update index.ts

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -1,18 +1,24 @@
 import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type { UserFacingSocketConfig } from '../Types'
+import { installConsoleFilter } from '../Utils/console-filter'
+import { printBanner } from '../Utils/banner'
 import { makeCommunitiesSocket } from './communities'
 
 
 const makeWASocket = (config: UserFacingSocketConfig) => {
+	installConsoleFilter()
+	printBanner()
+
 	const newConfig = {
 		...DEFAULT_CONNECTION_CONFIG,
 		...config
 	}
 
-	if (config.version && config.syncWaWebVersion === undefined) {
+		if (config.version && config.syncWaWebVersion === undefined) {
 		newConfig.syncWaWebVersion = false
 	}
 
+		
 	if (!newConfig.version) {
 		newConfig.version = DEFAULT_CONNECTION_CONFIG.version
 	}
