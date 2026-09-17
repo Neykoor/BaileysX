978f138fadb22e2811fd8bbeaa424df91e375b1d
Neykoor <cafecafirexos@gmail.com>
2026-09-17 07:41:57 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export * from './Store/index'
 export * from './Types/index'
 export * from './Defaults/index'
 export * from './WABinary/index'
-export * from './WAM/index'
+export { BinaryInfo } from './WAM/BinaryInfo'
 export * from './WAUSync/index'
 
 export type WASocket = ReturnType<typeof makeWASocket>
